d26ab5159663a4b7e89b587f46a01ecc659fb314
inclyc <me@inclyc.cn>
2022-06-20 17:37:53 +0800

    primes finish

--- user/primes.c
+++ user/primes.c
@@ -3,26 +3,27 @@ void produce(int backward_pipe_fd) {
   int prime;
   read(backward_pipe_fd, &prime, sizeof(int));
   printf("prime %d\n", prime);
-  if (prime > 10) {
+  if (prime >= 31) {
     return;
   }
-  int pid = fork();
   int pp[2];
   pipe(pp);
+  int pid = fork();
   if (pid == 0) {
     /* Child */
     close(pp[1]);
     produce(pp[0]);
     close(pp[0]);
   } else {
-    int forward;
-    close(pp[0]);
-    for (; read(backward_pipe_fd, &forward, sizeof(int));) {
-      write(pp[1], &forward, sizeof(int));
+    int n;
+    close(pp[0]); //  close a ----- b ----read--- c
+    for (; read(backward_pipe_fd, &n, sizeof(int));) {
+      if (n % prime)
+        write(pp[1], &n, sizeof(int));
     }
-    close(pp[1]);
+    close(pp[1]);            // close a ---------- b ---write-- c
+    close(backward_pipe_fd); // close a ---read--- b ---------- c
     wait(0);
-    close(backward_pipe_fd);
   }
   exit(0);
 }
@@ -38,7 +39,7 @@ int main() {
     produce(pp[0]);
   } else {
     close(pp[0]);
-    for (int i = 3; i < 32; i++) {
+    for (int i = 3; i < 32; i += 2) {
       write(pp[1], &i, sizeof(int));
     }
     close(pp[1]);
