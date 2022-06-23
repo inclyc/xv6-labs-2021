e4f178a2eed5f9c25a44aeec804bf0b25f34dec6
inclyc <me@inclyc.cn>
2022-06-24 03:43:08 +0800

    pgaccess

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -46,7 +46,7 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
-  
+
   addr = myproc()->sz;
   if(growproc(n) < 0)
     return -1;
@@ -81,6 +81,27 @@ int
 sys_pgaccess(void)
 {
   // lab pgtbl: your code here.
+  uint64 va_start;
+  int n_pages; // how many pages should we check
+  uint64 abits; // pointer to result buffer;
+  uint32 result = 0;
+  argaddr(0, &va_start);
+  argint(1, &n_pages);
+  argaddr(2, &abits);
+  if(va_start >= MAXVA || va_start < 0) return -1;
+  if(n_pages > 32 || n_pages < 0) return -1;
+  if(abits >= MAXVA || abits < 0) return -1;
+
+  pagetable_t pgtbl = myproc()->pagetable;
+  for(int i = 0;i < n_pages; i++){
+    pte_t *pte = walk(pgtbl, va_start + PGSIZE * i, 0);
+    if(!pte) return -1;
+    if(*pte & PTE_A){
+      result |= (1 << i);
+      *pte ^= PTE_A; // set access bit to zero
+    }
+  }
+  copyout(pgtbl, (uint64)abits, (char *)&result, sizeof(int));
   return 0;
 }
 #endif
